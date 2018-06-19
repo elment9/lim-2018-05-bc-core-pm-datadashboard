cbb89ca1152f3523e0f7518744cfa687826f3f6b
elment9 <elizabethmendoza.unmsm@gmail.com>
2018-06-19 18:29:50 -0500

    entendiendo el fetch

--- src/main.js
+++ src/main.js
@@ -1,21 +1,4 @@
-const sedeCohort = document.getElementById('sede');
-fetch('../data/cohorts.json')
-  .then(response => response.json())
-  .then(json => {
-    const sedes = json;
-    // console.log(sedes.length);
-    for (let i = 0; i < sedes.length; i++) {
-      const optionElements = document.createElement('option');
-      const contenidoOption = document.createTextNode(sedes[i].id);
-      optionElements.appendChild(contenidoOption);
-      console.log(sedes[i].id);
-      sedeCohort.appendChild(optionElements);
-      sedeCohort.setAttribute('value', sedes[i].id);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+
 
   // sedeCohort.addEventListener("change", (e) => {
   //   if (e.target.value === 'lim-2018-03-pre-core-pw') {
