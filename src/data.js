703478d34f876758431c47cc1980b2c6bd1e9d23
Elizabeth <elizabethmendoza.unmsm@gmail.com>
2018-06-28 10:30:47 -0500

    1ra FUNCIÓN: Fetch anidado e inicio de for each

--- src/data.js
+++ src/data.js
@@ -1,54 +1,54 @@
-//Creando el objeto data
-window.data = {
-};
+// //Creando el objeto data
+// window.data = {
+// };
 
-// variables url json
-const cohortTotal = '../data/cohorts.json',
-      usersLima = '../data/cohorts/lim-2018-03-pre-core-pw/users.json',
-      progressLima = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+// // variables url json
+// const cohortTotal = '../data/cohorts.json',
+//       usersLima = '../data/cohorts/lim-2018-03-pre-core-pw/users.json',
+//       progressLima = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
-// seleccionando elementos del html
-const sedeCohort = document.getElementById('sede');
+// // seleccionando elementos del html
+// const sedeCohort = document.getElementById('sede');
 
-//Traer cohorts
-fetch(cohortTotal)
-  .then(response => response.json())
-  .then(jsonCohortLim => {
-    const sedes = jsonCohortLim;
-    for (let i = 0; i < sedes.length; i++) {
-      const optionElements = document.createElement('option');
-      const contenidoOption = document.createTextNode(sedes[i].id);
-      optionElements.appendChild(contenidoOption);
-      console.log(sedes[i].id);
-      sedeCohort.appendChild(optionElements);
-      sedeCohort.setAttribute('value', sedes[i].id); //Atributo valor a sedes
-    }
-  })
-  .catch((err) => {//Error 404
-    console.error(err);
-  });
+// //Traer cohorts
+// fetch(cohortTotal)
+//   .then(response => response.json())
+//   .then(jsonCohortLim => {
+//     const sedes = jsonCohortLim;
+//     for (let i = 0; i < sedes.length; i++) {
+//       const optionElements = document.createElement('option');
+//       const contenidoOption = document.createTextNode(sedes[i].id);
+//       optionElements.appendChild(contenidoOption);
+//       console.log(sedes[i].id);
+//       sedeCohort.appendChild(optionElements);
+//       sedeCohort.setAttribute('value', sedes[i].id); //Atributo valor a sedes
+//     }
+//   })
+//   .catch((err) => {//Error 404
+//     console.error(err);
+//   });
 
-let myFunction = (e) => {
+// let myFunction = (e) => {
 
-  if (e.target.value === 'lim-2018-03-pre-core-pw') {//e.target = valor que se da cuando cliqueas opcion del selec
-    fetch(usersLima)
-      .then(response => response.json())
-      .then(jsonUsersLima => {
-          const studentsLima =jsonUsersLima.filter(function (studentFilter){//Creo una constante studentsLima y coloco json de cohort dentro. Luego uso filter (dentro coloco función). Si cumple esta función haz esto
-          if ((studentFilter.signupCohort === 'lim-2018-03-pre-core-pw') && (studentFilter.role === 'student')){//Filtramos de acuerdo a propiedades de lista de users
-          const nuevoParrafo = document.createElement('p');//creo elemento p
-          nuevoParrafo.innerText = studentFilter.name;
-          listStudent.appendChild(nuevoParrafo);
-          console.log();
-          }
-          else {
-            return false
-          }
-          })
+//   if (e.target.value === 'lim-2018-03-pre-core-pw') {//e.target = valor que se da cuando cliqueas opcion del selec
+//     fetch(usersLima)
+//       .then(response => response.json())
+//       .then(jsonUsersLima => {
+//           const studentsLima =jsonUsersLima.filter(function (studentFilter){//Creo una constante studentsLima y coloco json de cohort dentro. Luego uso filter (dentro coloco función). Si cumple esta función haz esto
+//           if ((studentFilter.signupCohort === 'lim-2018-03-pre-core-pw') && (studentFilter.role === 'student')){//Filtramos de acuerdo a propiedades de lista de users
+//           const nuevoParrafo = document.createElement('p');//creo elemento p
+//           nuevoParrafo.innerText = studentFilter.name;
+//           listStudent.appendChild(nuevoParrafo);
+//           console.log();
+//           }
+//           else {
+//             return false
+//           }
+//           })
 
 
-  })
-}
-}
-sedeCohort.addEventListener('change', myFunction);
+//   })
+// }
+// }
+// sedeCohort.addEventListener('change', myFunction);
 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
+const cohortTotal = '../data/cohorts.json',
+      usersLima = '../data/cohorts/lim-2018-03-pre-core-pw/users.json',
+      progressLima = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
-  // sedeCohort.addEventListener("change", (e) => {
-  //   if (e.target.value === 'lim-2018-03-pre-core-pw') {
-  //     fetch ('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
-  //     .then(response => response.json())
-  //     .then(json => {
-  //       const usersStudents = json;
-  //       console.log(usersStudents.length);
-  //       for (let i = 0; i < usersStudents.length; i++){
-  //         const p = document.createElement('p');
-  //         p.innerText = usersStudents[i].name;
-  //         listStudent.appendChild(p);
-  //       }
-  //     })
-  //   }
-  // })
+const getData = (callback) => {
+  fetch(usersLima)
+    .then((responseU) => {
+      fetch(progressLima)
+        .then((responseP) => {
+          fetch(cohortTotal)
+            .then((responseC) => {
+              Promise.all([responseU.json(), responseP.json(), responseC.json()])
+                  .then(dataArr => {
+                    console.log(dataArr);
+                  const [x, y, z] = dataArr;//destructuring de los arrays
+                  callback && callback(x, y, z);//llama a la función
+                })
+            })
+        })
+    })
+ }
+
+ 
+
+  const callbackGetData = (users,progress,cohorts) => {
+    const progressUser = Object.keys(progress);
+      console.log(progressUser.length);
+      console.log(users.length);
+      users.forEach(user => {
+        console.log(progress[user.id]);
+      });
+  } 
+getData(callbackGetData) 
+ 
+ 
